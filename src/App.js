42b71472f374f28281d3a329f56c13794725248d
François Parmentier <francois.parmentier@gmail.com>
2018-05-02 23:33:39 +0200

    Visualize the ConceptNetwork

--- src/App.js
+++ src/App.js
@@ -1,17 +1,92 @@
 import React, { Component } from 'react';
+import { InteractiveForceGraph, ForceGraphNode, ForceGraphArrowLink } from 'react-vis-force';
 import logo from './Robot.svg';
 import './App.css';
 
 const backend = process.env.REACT_APP_BACKEND || 'http://localhost:5000';
+const cnURL = `${backend}/v1/concept-network`;
+
+class ConceptNetwork extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cn: {},
+      cns: {},
+      stateURL: `${backend}/v1/concept-network-state/${props.username}`
+    }
+
+    fetch(this.state.stateURL)
+    .then(response => response.json())
+    .then(json => {
+      return this.setState({
+        ...this.state,
+        cns: json
+      });
+    })
+
+    fetch(cnURL)
+    .then(response => response.json())
+    .then(json => {
+      return this.setState({
+        ...this.state,
+        cn: json
+      })
+    });
+  }
+
+  render() {
+    const { cn, cns } = this.state;
+    const ready = Object.keys(cn).length && Object.keys(cns).length;
+    console.log(cn, cns)
+    return (
+      ready &&
+      <InteractiveForceGraph
+        zoom
+        simulationOptions={{ height: 300, width: 450, animate: true }}
+        labelAttr="label" showLabels
+        highlightDependencies
+      >
+        {Object.keys(cn.node)
+          .map(id => {
+            const { label, occ } = cn.node[id];
+            return <ForceGraphNode
+              key={id}
+              node={{
+                id,
+                label: label.slice(1),
+                weight: occ
+              }}
+              fill={
+                label.startsWith('w')
+                ? "red"
+                : "blue"}
+            />
+          })
+        }
+        {Object.keys(cn.link)
+          .map(linkId => {
+            const { fromId, toId, coOcc } = cn.link[linkId];
+            return <ForceGraphArrowLink key={linkId} link={{ source: fromId, target: toId, weight: coOcc }} />
+          })
+
+        }
+      </InteractiveForceGraph>
+    );
+  }
+}
 
 const PastEntries = ({list}) => (
-  <ul style={{textAlign: 'left'}}>
+  <ul style={{ textAlign: 'left' }}>
     {list
       .sort((a, b) => a.created - b.created)
       .reverse()
       .slice(0,10)
       .map((entry, i) => (
-      <li key={i}>{entry.utterer}: {entry.value}</li>
+      <li
+        key={i}
+        style={{backgroundColor: (entry.utterer === 'ECTOR' ? '#F8F8F8' : 'white')}}
+      >{entry.utterer}: {entry.value}</li>
     ))}
     {list.length > 10 &&
       <li>...</li>
@@ -96,6 +171,7 @@ class App extends Component {
         <User onChange={this.changeUser} name={this.state.username} />
         <Entry onKeyUp={this.treatEntry} />
         <PastEntries list={this.state.entries}/>
+        <ConceptNetwork username={this.state.username} />
       </div>
     );
   }
